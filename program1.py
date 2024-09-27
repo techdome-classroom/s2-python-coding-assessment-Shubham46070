719bace0ac7210ef2613a08cc30f997048a0ac23
Sanu Kumar <sanu848114@gmail.com>
2024-09-27 14:22:26 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -2,5 +2,11 @@ class Solution(object):
     def isValid(self, s):
         if len(s)%2:
             return False
-        l = len(s)//2
-        
+        left, right = 0, len(s)-1
+        while left<right:
+            if s[left]!=s[right]:
+                return False
+        return True
+
+if __name__ == "__main__":
+    print(Solution.isValid())
