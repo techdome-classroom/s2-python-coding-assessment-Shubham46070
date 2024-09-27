d49d453e25271bf2b998d5e88c289e034ebfff1f
Sanu Kumar <sanu848114@gmail.com>
2024-09-27 14:27:05 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -1,11 +1,13 @@
 class Solution(object):
     def romanToInt(self, s):
-        d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-        total = 0
-        for i in range(len(s)-1):
-            if d[s[i]]<d[s[i+1]]:
-                total-=d[s[i]]
-            else:
-                total+=d[s[i]]
-        total+=d[s[-1]]
-        return total
+        roman_to_integer = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000,
+        }
+        s = s.replace("IV", "IIII").replace("IX", "VIIII").replace("XL", "XXXX").replace("XC", "LXXXX").replace("CD", "CCCC").replace("CM", "DCCCC")
+        return sum(map(lambda x: roman_to_integer[x], s))
