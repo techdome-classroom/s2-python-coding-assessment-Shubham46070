a8d7b32e9a517bf431389cc8341a8993f4beb4b6
Sanu Kumar <sanu848114@gmail.com>
2024-09-27 14:16:16 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -1,4 +1,11 @@
 class Solution(object):
     def romanToInt(self, s):
         d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-        return sum((-1 if d[s[i]] < d[s[i + 1]] else 1) * d[s[i]] for i in range(len(s) - 1)) + d[s[-1]]
+        total = 0
+        for i in range(len(s)-1):
+            if d[s[i]]<d[s[i+1]]:
+                total-=d[s[i]]
+            else:
+                total+=d[s[i]]
+        total+=d[s[-1]]
+        return total
