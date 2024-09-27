d251035763f6ec6e6d414f23cd5e2a1672e96ade
Sanu Kumar <sanu848114@gmail.com>
2024-09-27 14:24:07 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -1,6 +1,9 @@
 class Solution(object):
     def isValid(self, s):
         stack = []
+        for c in s:
+            if c in '({[':
+                stack.append(c)
 
 if __name__ == "__main__":
     print(Solution.isValid())
